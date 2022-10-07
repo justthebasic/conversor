e7e8d6d3f760e3889ad5203ae9d8cef6c44adac3
Erick <ericklamartinne0800@gmail.com>
2022-10-07 13:20:23 -0300

    bin e hex

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function converter (num) {
+function converterbin (num) {
     var dec = parseInt(num)
     var bin = dec.toString(2)
     document.getElementById('res').innerText=bin
+}
+
+function converterhex (num) {
+    var dec = parseInt(num)
+    var hex = dec.toString(16)
+    document.getElementById('res2').innerText=hex
 }
